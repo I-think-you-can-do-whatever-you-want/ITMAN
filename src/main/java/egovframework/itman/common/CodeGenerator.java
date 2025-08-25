5e0125aa55f42f64ed65679b01efde08eb75e823
SUUUPERKENTA <minseo00515@naver.com>
2025-08-26 03:11:32 +0900

    공유 코드용 랜덤 코드생성기

---
+++ src/main/java/egovframework/itman/common/CodeGenerator.java
@@ -0,0 +1,16 @@
+package egovframework.itman.common;
+
+import java.security.SecureRandom;
+
+public class CodeGenerator {
+    private static final String CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateCode(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARSET.charAt(random.nextInt(CHARSET.length())));
+        }
+        return sb.toString();
+    }
+}
